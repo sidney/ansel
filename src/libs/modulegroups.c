cef83348155f08fc728dcc14b99a20b8aa9df2af
Aurélien PIERRE <aurelien@aurelienpierre.com>
2023-02-25 01:57:04 +0100

    Rewrite module groups as basic text tabs

--- src/libs/modulegroups.c
+++ src/libs/modulegroups.c
@@ -29,7 +29,6 @@
 
 DT_MODULE(1)
 
-#define PADDING 2
 #define DT_IOP_ORDER_INFO (darktable.unmuted & DT_DEBUG_IOPORDER)
 
 #include "modulegroups.h"
@@ -37,14 +36,13 @@ DT_MODULE(1)
 typedef struct dt_lib_modulegroups_t
 {
   uint32_t current;
-  GtkWidget *buttons[DT_MODULEGROUP_SIZE];
+  GtkWidget *notebook;
   GtkWidget *text_entry;
-  GtkWidget *hbox_buttons;
   GtkWidget *hbox_search_box;
 } dt_lib_modulegroups_t;
 
 /* toggle button callback */
-static void _lib_modulegroups_toggle(GtkWidget *button, gpointer data);
+static void _lib_modulegroups_toggle(GtkNotebook *notebook, GtkWidget *page, guint page_num, gpointer user_data);
 /* helper function to update iop module view depending on group */
 static void _lib_modulegroups_update_iop_visibility(dt_lib_module_t *self);
 
@@ -106,6 +104,9 @@ int position()
 
 static void _text_entry_changed_callback(GtkEntry *entry, dt_lib_module_t *self)
 {
+  dt_lib_modulegroups_t *d = (dt_lib_modulegroups_t *)self->data;
+  const gchar *text_entered = gtk_entry_get_text(entry);
+  gtk_widget_set_sensitive(d->notebook, !(text_entered && text_entered[0] != '\0'));
   _lib_modulegroups_update_iop_visibility(self);
 }
 
@@ -114,16 +115,19 @@ static gboolean _text_entry_icon_press_callback(GtkEntry *entry, GtkEntryIconPos
 {
   dt_lib_modulegroups_t *d = (dt_lib_modulegroups_t *)self->data;
   gtk_entry_set_text(GTK_ENTRY(d->text_entry), "");
+  gtk_widget_set_sensitive(d->notebook, TRUE);
   return TRUE;
 }
 
 static gboolean _text_entry_key_press_callback(GtkWidget *widget, GdkEventKey *event, gpointer user_data)
 {
-
   if(event->keyval == GDK_KEY_Escape)
   {
+    dt_lib_module_t *self = (dt_lib_module_t *)user_data;
+    dt_lib_modulegroups_t *d = (dt_lib_modulegroups_t *)self->data;
     gtk_entry_set_text(GTK_ENTRY(widget), "");
     gtk_widget_grab_focus(dt_ui_center(darktable.gui->ui));
+    gtk_widget_set_sensitive(d->notebook, TRUE);
     return TRUE;
   }
 
@@ -158,64 +162,11 @@ void gui_init(dt_lib_module_t *self)
   dt_gui_add_help_link(self->widget, dt_get_help_url(self->plugin_name));
   gtk_widget_set_name(self->widget, "modules-tabs");
 
-  dtgtk_cairo_paint_flags_t pf = CPF_NONE;
-
-  d->hbox_buttons = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 0);
-  d->hbox_search_box = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 0);
-
-  /* active */
-  d->buttons[DT_MODULEGROUP_ACTIVE_PIPE] = dtgtk_togglebutton_new(dtgtk_cairo_paint_modulegroup_active, pf, NULL);
-  g_signal_connect(d->buttons[DT_MODULEGROUP_ACTIVE_PIPE], "toggled", G_CALLBACK(_lib_modulegroups_toggle),
-                   self);
-  gtk_widget_set_tooltip_text(d->buttons[DT_MODULEGROUP_ACTIVE_PIPE], _("show only active modules"));
-
-  /* basic */
-  int g_index = DT_MODULEGROUP_BASIC;
-  d->buttons[g_index] = dtgtk_togglebutton_new(dtgtk_cairo_paint_modulegroup_basic, pf, NULL);
-  g_signal_connect(d->buttons[g_index], "toggled", G_CALLBACK(_lib_modulegroups_toggle), self);
-  gtk_widget_set_tooltip_text(d->buttons[g_index], _("basic group"));
-
-  /* tone */
-  g_index = DT_MODULEGROUP_TONE;
-  d->buttons[g_index] = dtgtk_togglebutton_new(dtgtk_cairo_paint_modulegroup_tone, pf, NULL);
-  g_signal_connect(d->buttons[g_index], "toggled", G_CALLBACK(_lib_modulegroups_toggle), self);
-  gtk_widget_set_tooltip_text(d->buttons[g_index], _("tone group"));
-
-  /* color */
-  g_index = DT_MODULEGROUP_COLOR;
-  d->buttons[g_index] = dtgtk_togglebutton_new(dtgtk_cairo_paint_modulegroup_color, pf, NULL);
-  g_signal_connect(d->buttons[g_index], "toggled", G_CALLBACK(_lib_modulegroups_toggle), self);
-  gtk_widget_set_tooltip_text(d->buttons[g_index], _("color group"));
-
-  /* correct */
-  g_index = DT_MODULEGROUP_CORRECT;
-  d->buttons[g_index] = dtgtk_togglebutton_new(dtgtk_cairo_paint_modulegroup_correct, pf, NULL);
-  g_signal_connect(d->buttons[g_index], "toggled", G_CALLBACK(_lib_modulegroups_toggle), self);
-  gtk_widget_set_tooltip_text(d->buttons[g_index], _("correction group"));
-
-  /* effect */
-  g_index = DT_MODULEGROUP_EFFECT;
-  d->buttons[g_index] = dtgtk_togglebutton_new(dtgtk_cairo_paint_modulegroup_effect, pf, NULL);
-  g_signal_connect(d->buttons[g_index], "toggled", G_CALLBACK(_lib_modulegroups_toggle), self);
-  gtk_widget_set_tooltip_text(d->buttons[g_index], _("effects group"));
-
-  /*
-   * layout button row
-   */
-  GtkWidget *br = d->hbox_buttons;
-  for(int k = 0; k < DT_MODULEGROUP_SIZE; k++)
-  {
-    gtk_box_pack_start(GTK_BOX(br), d->buttons[k], TRUE, TRUE, 0);
-  }
-
   /* search box */
-  GtkWidget *label = gtk_label_new(_("search"));
-  gtk_box_pack_start(GTK_BOX(d->hbox_search_box), label, FALSE, TRUE, 0);
-
+  d->hbox_search_box = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 0);
   d->text_entry = gtk_entry_new();
+  gtk_entry_set_placeholder_text(GTK_ENTRY(d->text_entry), _("Search a module…"));
   gtk_widget_add_events(d->text_entry, GDK_FOCUS_CHANGE_MASK);
-
-  gtk_widget_set_tooltip_text(d->text_entry, _("search modules by name or tag"));
   gtk_widget_add_events(d->text_entry, GDK_KEY_PRESS_MASK);
   g_signal_connect(G_OBJECT(d->text_entry), "changed", G_CALLBACK(_text_entry_changed_callback), self);
   g_signal_connect(G_OBJECT(d->text_entry), "icon-press", G_CALLBACK(_text_entry_icon_press_callback), self);
@@ -225,18 +176,30 @@ void gui_init(dt_lib_module_t *self)
   gtk_entry_set_icon_from_icon_name(GTK_ENTRY(d->text_entry), GTK_ENTRY_ICON_SECONDARY, "edit-clear");
   gtk_entry_set_icon_tooltip_text(GTK_ENTRY(d->text_entry), GTK_ENTRY_ICON_SECONDARY, _("clear text"));
   gtk_widget_set_name(GTK_WIDGET(d->hbox_search_box), "search-box");
-
-
-  gtk_box_pack_start(GTK_BOX(self->widget), d->hbox_buttons, TRUE, TRUE, 0);
   gtk_box_pack_start(GTK_BOX(self->widget), d->hbox_search_box, TRUE, TRUE, 0);
 
-  gtk_toggle_button_set_active(GTK_TOGGLE_BUTTON(d->buttons[d->current]), TRUE);
+  /* Tabs */
+  d->notebook = GTK_WIDGET(gtk_notebook_new());
+  char *labels[DT_MODULEGROUP_SIZE] = { _("Enabled"),  _("Optics"), _("Grading"),
+                _("Corrections"), _("Effects"),  _("Scan") };
+  char *tooltips[DT_MODULEGROUP_SIZE] = { _("Enabled"),  _("Grading"), _("Effects"),
+                _("Effects"), _("Effects"),  _("Effects") };
+
+  for(int i = 0; i < DT_MODULEGROUP_SIZE; i++)
+  {
+    GtkWidget *label = gtk_label_new(labels[i]);
+    gtk_widget_set_tooltip_text(label, tooltips[i]);
+    GtkWidget *page = gtk_box_new(GTK_ORIENTATION_VERTICAL, 0);
+    gtk_notebook_append_page(GTK_NOTEBOOK(d->notebook), page, label);
+  }
+  gtk_notebook_popup_enable(GTK_NOTEBOOK(d->notebook));
+  gtk_notebook_set_scrollable(GTK_NOTEBOOK(d->notebook), TRUE);
+  g_signal_connect(G_OBJECT(d->notebook), "switch_page", G_CALLBACK(_lib_modulegroups_toggle), self);
+
+  gtk_box_pack_start(GTK_BOX(self->widget), GTK_WIDGET(d->notebook), TRUE, TRUE, 0);
+
   if(d->current == DT_MODULEGROUP_NONE) _lib_modulegroups_update_iop_visibility(self);
   gtk_widget_show_all(self->widget);
-  gtk_widget_show_all(d->hbox_buttons);
-  gtk_widget_set_no_show_all(d->hbox_buttons, TRUE);
-  gtk_widget_show_all(d->hbox_search_box);
-  gtk_widget_set_no_show_all(d->hbox_search_box, TRUE);
 
   /*
    * set the proxy functions
@@ -307,21 +270,6 @@ static void _lib_modulegroups_update_iop_visibility(dt_lib_module_t *self)
   if (DT_IOP_ORDER_INFO)
     fprintf(stderr,"\n^^^^^ modulegroups");
 
-  /* only show module group as selected if not currently searching */
-  if(d->current != DT_MODULEGROUP_NONE)
-  {
-    const int cb = _lib_modulegroups_get(self);
-    /* toggle button visibility without executing callback */
-    g_signal_handlers_block_matched(d->buttons[cb], G_SIGNAL_MATCH_FUNC, 0, 0, NULL, _lib_modulegroups_toggle, NULL);
-
-    if(text_entered && text_entered[0] != '\0')
-      gtk_toggle_button_set_active(GTK_TOGGLE_BUTTON(d->buttons[cb]), FALSE);
-    else
-      gtk_toggle_button_set_active(GTK_TOGGLE_BUTTON(d->buttons[cb]), TRUE);
-
-    g_signal_handlers_unblock_matched(d->buttons[cb], G_SIGNAL_MATCH_FUNC, 0, 0, NULL, _lib_modulegroups_toggle, NULL);
-  }
-
   GList *modules = darktable.develop->iop;
   if(modules)
   {
@@ -342,15 +290,6 @@ static void _lib_modulegroups_update_iop_visibility(dt_lib_module_t *self)
 
       /* skip modules without an gui */
       if(dt_iop_is_hidden(module)) continue;
-
-      // do not show non-active modules
-      // we don't want the user to mess with those
-      if(FALSE) // (module->iop_order == INT_MAX)
-      {
-        if(darktable.develop->gui_module == module) dt_iop_request_focus(NULL);
-        if(w) gtk_widget_hide(w);
-        continue;
-      }
 
       // if there's some search text show matching modules only
       if(text_entered && text_entered[0] != '\0')
@@ -428,39 +367,15 @@ static void _lib_modulegroups_update_iop_visibility(dt_lib_module_t *self)
   dt_dev_modules_update_multishow(darktable.develop);
 }
 
-static void _lib_modulegroups_toggle(GtkWidget *button, gpointer user_data)
+static void _lib_modulegroups_toggle(GtkNotebook *notebook, GtkWidget *page, guint page_num, gpointer user_data)
 {
   dt_lib_module_t *self = (dt_lib_module_t *)user_data;
   dt_lib_modulegroups_t *d = (dt_lib_modulegroups_t *)self->data;
-  const gchar *text_entered = gtk_entry_get_text(GTK_ENTRY(d->text_entry));
 
-  /* block all button callbacks */
-  for(int k = 0; k < DT_MODULEGROUP_SIZE; k++)
-    g_signal_handlers_block_matched(d->buttons[k], G_SIGNAL_MATCH_FUNC, 0, 0, NULL, _lib_modulegroups_toggle,
-                                    NULL);
-
-  /* deactivate all buttons */
-  uint32_t cb = 0;
-  for(int k = 0; k < DT_MODULEGROUP_SIZE; k++)
-  {
-    /* store toggled modulegroup */
-    if(d->buttons[k] == button) cb = k;
-    gtk_toggle_button_set_active(GTK_TOGGLE_BUTTON(d->buttons[k]), FALSE);
-  }
-
-  /* only deselect button if not currently searching else re-enable module */
-  if(d->current == cb && !(text_entered && text_entered[0] != '\0'))
-    d->current = DT_MODULEGROUP_NONE;
+  if(d->current == page_num)
+    return; // nothing to do
   else
-  {
-    d->current = cb;
-    gtk_toggle_button_set_active(GTK_TOGGLE_BUTTON(d->buttons[cb]), TRUE);
-  }
-
-  /* unblock all button callbacks */
-  for(int k = 0; k < DT_MODULEGROUP_SIZE; k++)
-    g_signal_handlers_unblock_matched(d->buttons[k], G_SIGNAL_MATCH_FUNC, 0, 0, NULL,
-                                      _lib_modulegroups_toggle, NULL);
+    d->current = page_num;
 
   /* clear search text */
   g_signal_handlers_block_matched(d->text_entry, G_SIGNAL_MATCH_FUNC, 0, 0, NULL, _text_entry_changed_callback, NULL);
@@ -484,10 +399,13 @@ static gboolean _lib_modulegroups_set_gui_thread(gpointer user_data)
   dt_lib_modulegroups_t *d = (dt_lib_modulegroups_t *)params->self->data;
 
   /* set current group and update visibility */
-  if(params->group < DT_MODULEGROUP_SIZE && GTK_IS_TOGGLE_BUTTON(d->buttons[params->group]))
-    gtk_toggle_button_set_active(GTK_TOGGLE_BUTTON(d->buttons[params->group]), TRUE);
-  _lib_modulegroups_update_iop_visibility(params->self);
+  if(params->group < DT_MODULEGROUP_SIZE && GTK_IS_NOTEBOOK(d->notebook))
+  {
+    d->current = params->group;
+    gtk_notebook_set_current_page(GTK_NOTEBOOK(d->notebook), params->group);
+  }
 
+  _lib_modulegroups_update_iop_visibility(params->self);
   free(params);
   return FALSE;
 }
@@ -572,7 +490,6 @@ static uint32_t _lib_modulegroups_get(dt_lib_module_t *self)
   return DT_MODULEGROUP_NONE;
 }
 
-#undef PADDING
 // modelines: These editor modelines have been set for all relevant files by tools/update_modelines.sh
 // vim: shiftwidth=2 expandtab tabstop=2 cindent
 // kate: tab-indents: off; indent-width 2; replace-tabs on; indent-mode cstyle; remove-trailing-spaces modified;
