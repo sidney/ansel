c2b7106a25b17e014965302ad5d41b41c1d02b50
Aurélien PIERRE <aurelien@aurelienpierre.com>
2023-03-02 12:39:16 +0100

    Global menu : add links to help

--- src/gui/actions/help.h
+++ src/gui/actions/help.h
@@ -14,10 +14,6 @@ static void show_about_dialog()
                                 _("Organize and develop images from digital cameras"));
   gtk_about_dialog_set_website(GTK_ABOUT_DIALOG(dialog), "https://ansel.photos");
   gtk_about_dialog_set_website_label(GTK_ABOUT_DIALOG(dialog), _("Website"));
-  /*
-  gtk_about_dialog_set_website(GTK_ABOUT_DIALOG(dialog), "https://liberapay.com/aurelienpierre");
-  gtk_about_dialog_set_website_label(GTK_ABOUT_DIALOG(dialog), _("Donate"));
-  */
   char *icon = g_strdup("ansel");
   gtk_about_dialog_set_logo_icon_name(GTK_ABOUT_DIALOG(dialog), icon);
   g_free(icon);
@@ -35,8 +31,29 @@ static void show_about_dialog()
   gtk_widget_destroy(dialog);
 }
 
+void open_doc_callback(GtkWidget *widget)
+{
+  // TODO: use translated URL when doc gets translated
+  gtk_show_uri_on_window(GTK_WINDOW(dt_ui_main_window(darktable.gui->ui)), "https://ansel.photos/en/doc", GDK_CURRENT_TIME, NULL);
+}
+
+void open_booking_callback(GtkWidget *widget)
+{
+  gtk_show_uri_on_window(GTK_WINDOW(dt_ui_main_window(darktable.gui->ui)),
+  "https://photo.aurelienpierre.com/private-lessons-on-retouching-with-darktable/?lang=en", GDK_CURRENT_TIME, NULL);
+}
+
+void open_donate_callback(GtkWidget *widget)
+{
+  // TODO: use translated URL when doc gets translated
+  gtk_show_uri_on_window(GTK_WINDOW(dt_ui_main_window(darktable.gui->ui)), "https://liberapay.com/aurelienpierre", GDK_CURRENT_TIME, NULL);
+}
 
 void append_help(GtkWidget **menus, GList **lists, const dt_menus_t index)
 {
+  add_sub_menu_entry(menus, lists, _("Online documentation"), index, NULL, open_doc_callback, NULL, NULL, NULL);
+  add_sub_menu_entry(menus, lists, _("Book a training session"), index, NULL, open_booking_callback, NULL, NULL, NULL);
+  add_menu_separator(menus[index]);
+  add_sub_menu_entry(menus, lists, _("Donate"), index, NULL, open_donate_callback, NULL, NULL, NULL);
   add_sub_menu_entry(menus, lists, _("About"), index, NULL, show_about_dialog, NULL, NULL, NULL);
 }
