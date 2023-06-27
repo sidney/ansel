ff82b588278f1e1234c947dc83296ffe194ec150
Aurélien PIERRE <aurelien@aurelienpierre.com>
2023-06-27 12:11:50 +0200

    Make Lightroom input support optional, disable it by default
    LibXML2 version  2.11.4 deprecates the XML parser importing XMP from Lightroom.
    XMP should be handled through Exiv2 anyway.
    No time for that crap.

--- DefineOptions.cmake
+++ DefineOptions.cmake
@@ -34,6 +34,7 @@ option(BUILD_MSYS2_INSTALL "Build an MSYS2 version of the install, aka for Windo
 option(BUILD_NOISE_TOOLS "Build tools for generating noise profiles" OFF)
 option(BUILD_CURVE_TOOLS "Build tools for generating base and tone curves" OFF)
 option(USE_LENSFUN "Enable LensFun support" ON)
+option(USE_LIGHTROOM "Enable Adobe Lightroom XMP settings import support" OFF)
 option(USE_GMIC "Use G'MIC image processing framework." ON)
 option(USE_ICU "Use ICU - International Components for Unicode." ON)
 option(USE_GAME "Build 1st April easter egg game" ON)
