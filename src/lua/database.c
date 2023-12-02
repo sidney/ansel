a7d1a9f1698b5f6386e8dbd02604afc9ca96caff
Aurélien PIERRE <aurelien@aurelienpierre.com>
2023-12-02 16:44:57 +0100

    dt_import_image_lua: propagate API change into forgotten file

--- src/lua/database.c
+++ src/lua/database.c
@@ -149,7 +149,7 @@ static int import_images(lua_State *L)
       return luaL_error(L, "error while importing");
     }
 
-    result = dt_image_import_lua(new_film.id, full_name, TRUE);
+    result = dt_image_import_lua(new_film.id, full_name);
     if(dt_film_is_empty(new_film.id)) dt_film_remove(new_film.id);
     dt_film_cleanup(&new_film);
     if(result == 0)
@@ -323,4 +323,3 @@ int dt_lua_init_database(lua_State *L)
 // vim: shiftwidth=2 expandtab tabstop=2 cindent
 // kate: tab-indents: off; indent-width 2; replace-tabs on; indent-mode cstyle; remove-trailing-spaces modified;
 // clang-format on
-
