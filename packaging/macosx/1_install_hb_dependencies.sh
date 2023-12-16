1b9d4c582d8898618b4206f4c245b6344931323c
lologor <laurent@perraut.net>
2023-12-16 13:25:55 +0100

    Address issue exiv2 0.28 -> install 0.27.6

--- packaging/macosx/1_install_hb_dependencies.sh
+++ packaging/macosx/1_install_hb_dependencies.sh
@@ -19,6 +19,13 @@ fi
 brew update
 brew upgrade
 
+# Ensure you still have the version 0.27.6 of exiv2
+# Procedure:
+# brew remove exiv2 (if already installed)
+# brew tap homebrew/core
+# brew tap-new $USER/local-exiv2
+# brew extract --version=0.27.6 exiv2 $USER/local-exiv2
+
 # Define homebrew dependencies
 hbDependencies="adwaita-icon-theme \
     cmake \
@@ -26,7 +33,7 @@ hbDependencies="adwaita-icon-theme \
     cmocka \
     curl \
     desktop-file-utils \
-    exiv2 \
+    exiv2@0.27.6 \
     gettext \
     git \
     glib \
