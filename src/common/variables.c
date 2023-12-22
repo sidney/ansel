9da390a86c286804d7403657adb959e303191a01
Aurélien PIERRE <aurelien@aurelienpierre.com>
2023-12-22 19:26:53 +0100

    Add a FIXME comment on possible deadlock over image cache in variables expansion

--- src/common/variables.c
+++ src/common/variables.c
@@ -186,6 +186,7 @@ static void _init_expansion(dt_variables_params_t *params, gboolean iterate)
       }
     }
 
+    // FIXME: do we have a deadlock if params->img != NULL ??
     if(params->img == NULL) dt_image_cache_read_release(darktable.image_cache, img);
   }
   else if(params->data->exif_time[0])
