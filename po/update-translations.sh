dea7d112045851c23d75cd70df5275dddb5add77
Aurélien PIERRE <aurelien@aurelienpierre.com>
2024-01-02 00:43:54 +0100

    Update translation script: don't remove fuzzy tags

--- po/update-translations.sh
+++ po/update-translations.sh
@@ -4,7 +4,7 @@ intltool-update -p -g ansel
 
 # Remove old translations
 for f in *.po ; do
-  msgattrib --translated --no-fuzzy --no-obsolete -o $f $f
+  msgattrib --translated --no-obsolete -o $f $f
 done
 
 # Report
