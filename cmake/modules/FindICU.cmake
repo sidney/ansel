be66d0e46944598c84109dbd3438072eb20c9126
Sidney Markowitz <sidney@sidney.com>
2025-01-02 20:42:37 +1300

    Fix build with icu4c on macOS
    Signed-off-by: Sidney Markowitz <sidney@sidney.com>

--- cmake/modules/FindICU.cmake
+++ cmake/modules/FindICU.cmake
@@ -3,13 +3,20 @@
 #
 
 # This module defines
+# ICU_INCLUDE_DIRS, where to find *.h, etc
 # ICU_LIBRARIES, the libraries
 # ICU_FOUND, If false, do not try to use ICU.
 
 include(LibFindMacros)
 
 # Use pkg-config to get hints about paths
-libfind_pkg_check_modules(ICU_PKGCONF icu-i18n)
+libfind_pkg_check_modules(ICU_PKGCONF icu-i18n icu-uc)
+
+# Find include dir
+find_path(ICU_INCLUDE_DIR
+  NAMES unicode/utypes.h
+  HINTS ${IUC_PKGCONF_INCLUDE_DIRS}
+)
 
 # Find the libraries
 foreach(i ${ICU_PKGCONF_LIBRARIES})
@@ -18,12 +25,14 @@ foreach(i ${ICU_PKGCONF_LIBRARIES})
   unset(_ICU_LIBRARY CACHE)
 endforeach(i)
 
-if(ICU_LIBRARY)
+if(ICU_LIBRARY AND ICU_INCLUDE_DIR)
+  set(ICU_PROCESS_INCLUDES ${ICU_INCLUDE_DIR})
   set(ICU_PROCESS_LIBS ${ICU_LIBRARY})
   libfind_process(ICU)
-endif(ICU_LIBRARY)
+endif(ICU_LIBRARY AND ICU_INCLUDE_DIR)
 
 if(ICU_FOUND)
+  set(ICU_INCLUDE_DIRS ${ICU_INCLUDE_DIR})
   set(ICU_LIBRARIES ${ICU_LIBRARY})
 else()
   message(STATUS "ICU not found")
